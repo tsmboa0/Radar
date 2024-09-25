d9a9f48ecc767b1be047592c37b4eaf6320dcd8a
tsmboa0 <tsmboa@gmail.com>
2024-09-25 11:22:06 +0100

    added type ActionsJson to import

--- app/actions.json/route.ts
+++ app/actions.json/route.ts
@@ -1,27 +1,25 @@
-import { ACTIONS_CORS_HEADERS, ActionsJson, createActionHeaders } from "@solana/actions";
+import { createActionHeaders, type ActionsJson } from "@solana/actions";
 
+export const GET = async () => {
+  const payload: ActionsJson = {
+    rules: [
+      {
+        pathPattern: "/play/*",
+        apiPath: "/api/action/play/*",
+      },
+      // idempotent rule as the fallback
+      {
+        pathPattern: "/api/action/play/**",
+        apiPath: "/api/action/play/**",
+      },
+    ],
+  };
 
-const headers = createActionHeaders();
+  return Response.json(payload, {
+    headers: createActionHeaders(),
+  });
+};
 
-export const GET = async()=>{
-
-    const payload : ActionsJson ={
-        rules: [
-            {
-                pathPattern: "/play/*",
-                apiPath: "/api/action/play/*"
-            },
-            // idempotent rule as the fallback
-            {
-                pathPattern: "/api/actions/play/**",
-                apiPath: "/api/actions/play/**",
-            },
-        ]
-    } ;
-
-    return Response.json(payload, {
-        headers
-    })
-}
-
+// DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
+// THIS WILL ENSURE CORS WORKS FOR BLINKS
 export const OPTIONS = GET;
