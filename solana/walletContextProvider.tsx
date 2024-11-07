4ddd1ad30f4560292c602288ecd4ea10b28ff360
tsmboa0 <tsmboa@gmail.com>
2024-11-07 15:48:42 +0100

    fixed minor bug

--- solana/walletContextProvider.tsx
+++ solana/walletContextProvider.tsx
@@ -10,7 +10,7 @@ import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 const WalletContextProvider = ({ children, }: {children: React.ReactNode})=>{
-    const network = WalletAdapterNetwork.Mainnet
+    const network = WalletAdapterNetwork.Devnet;
     const endpoint = useMemo(()=>clusterApiUrl(network), [network]);
 
     const wallets = useMemo(()=>[
