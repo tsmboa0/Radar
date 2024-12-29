0d3c09aff358450c340e74e228b2703e964abf9f
tsmboa0 <tsmboa@gmail.com>
2024-12-29 21:10:32 +0100

    113.6c

--- mongodbUri.js
+++ mongodbUri.js
@@ -2,8 +2,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 const mongoPass = encodeURIComponent(process.env.MONGO_PASS);
-const uName = encodeURIComponent("tsmboa")
-const MONGO_DB_URI = `mongodb+srv://${uName}:${mongoPass}`+process.env.MONGO_DB_URI;
+const MONGO_DB_URI = `mongodb+srv://tsmboa:${mongoPass}`+process.env.MONGO_DB_URI;
 
 
 const Client = new MongoClient(MONGO_DB_URI, {
